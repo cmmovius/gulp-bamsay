7a98493afb59a3b8b261c77cf381bc87801e30cd
Christine Movius <christine.movius@gmail.com>
2016-04-18 15:30:13 -0400

    Install gulp sass. Compile sass into css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var gulp = require('gulp');
+var sass = require("gulp-sass");
 
 //define a task with the name of 'default'
 // and a callback to perform when the task is ran
@@ -22,4 +24,10 @@ gulp.task('jshint', function() {
   return gulp.src('./js/*.js')
   .pipe(jshint())
   .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('sass', function () {
+ return gulp.src('./css/**/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./css/'));
 });
